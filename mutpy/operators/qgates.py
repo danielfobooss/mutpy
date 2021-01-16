3aafc0d2c2232b230eada414d8b2208a7ae32f45
Rui Maranhao <rui@computer.org>
2021-01-16 06:18:13 -0800

    adding mutation to quantum gates

--- mutpy/operators/qgates.py
+++ mutpy/operators/qgates.py
@@ -1,20 +1,62 @@
 import ast
-
+from inspect import getmembers, isfunction, signature
 from mutpy.operators.base import MutationResign, MutationOperator
-     
+from qiskit import QuantumCircuit
+import random
+# https://qiskit.org/documentation/apidoc/circuit_library.html
+# https://github.com/Qiskit/qiskit-terra/blob/master/qiskit/circuit/quantumcircuit.py
+# https://quantumai.google/cirq/gates     
+
 class QuantumGateReplacement(MutationOperator):
-    def should_mutate(self, node):
-        with open('results1.txt', 'a') as f:
-                f.write("mutating")
+    def should_mutate(self, node):   
         return ast.Call
 
-    def mutate_FunctionDef(self, node):
-        with open('results2.txt', 'a') as f:
-                f.write("mutating")
-                f.write(node.name)
-        if node.name == "h":
-            with open('results2.txt', 'a') as f:
-                f.write(node.name)
-            return node
+    def mutate_Call(self, node):
+        if isinstance(node.func, ast.Name):
+            print("NAME")
+            print(ast.dump(node))
+
+        if isinstance(node.func, ast.Attribute):
+            print("Attribute: ", ast.dump(node))  
+            if node.func.attr in self.equivalent_gates():
+                print('Mutating quantum gate: ', node.func.attr)
+                equiv_gates = self.equivalent_gates()[node.func.attr]
+                equiv_gates.remove(node.func.attr)
+                if equiv_gates is None: return node
+
+                new_gate = random.choice(list(equiv_gates)) 
+                mutated_qgate = ast.Attribute(node.func.value, new_gate, node.func.ctx)
+                print("Mutated: ", ast.dump(ast.Call(mutated_qgate, node.args, node.keywords)))
+                return ast.Call(mutated_qgate, node.args, node.keywords)
         raise MutationResign()      
+
+
     
+
+    def compare_gate_functions(self, f1, f2, discard_named_args=True):
+        if not discard_named_args:
+            return signature(f1) == signature(f2)
+        return [arg for arg in signature(f1).parameters.values() if arg.default is arg.empty] == \
+            [arg for arg in signature(f2).parameters.values() if arg.default is arg.empty]
+
+    def equivalent_gates(self, discard_named_args=True):
+        existing_gate_names = ['ch', 'cp', 'cx', 'cy', 'cz', 'crx', 'cry', 'crz', 'ccx', 'cswap',
+                                'csx', 'cu', 'cu1', 'cu3', 'dcx', 'h', 'i', 'id', 'iden', 'iswap',
+                                'ms', 'p', 'r', 'rx', 'rxx', 'ry', 'ryy', 'rz', 'rzx', 'rzz', 's',
+                                'sdg', 'swap', 'sx', 'x', 'y', 'z', 't', 'tdg', 'u', 'u1', 'u2',
+                                'u3']
+        gate_functions = [o for o in getmembers(QuantumCircuit) if isfunction(o[1]) and o[0] in existing_gate_names]
+        gate_to_gate = { g: set() for g in existing_gate_names }
+        done = set()
+        for gate, func in gate_functions:
+            for gate_, func_ in gate_functions:
+                if gate == gate_:
+                    continue
+                if self.compare_gate_functions(func, func_, discard_named_args):
+                    gate_to_gate[gate].add(gate_)
+                    gate_to_gate[gate_].add(gate_)
+                    done.add(gate)
+                    done.add(gate_)
+        return gate_to_gate
+    
+        
