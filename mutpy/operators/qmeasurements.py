f739b18eaf0f1a450a5504db5007f7ec6b9c63be
danielfobooss <dani.fortuna97@gmail.com>
2021-03-30 01:57:28 +0100

    remove measurements working

--- mutpy/operators/qmeasurements.py
+++ mutpy/operators/qmeasurements.py
@@ -6,10 +6,10 @@ from mutpy.operators.base import MutationResign, MutationOperator
 class QuantumMeasurementRemove(MutationOperator):
 
     def should_mutate_Attribute(self, node):
-        return isinstance(node.value.func, ast.Attribute) and node.value.func.attr == "measure"
+        return isinstance(node.value, ast.Call) and isinstance(node.value.func, ast.Attribute) and node.value.func.attr == "measure"
 
     def should_mutate_Name(self, node):
-        return isinstance(node.value.func, ast.Name) and node.value.func.id == "measure"
+        return isinstance(node.value, ast.Call) and isinstance(node.value.func, ast.Name) and node.value.func.id == "measure"
 
     def mutate_Expr(self, node):
         if self.should_mutate_Name(node):
