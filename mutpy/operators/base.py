660764a116447130d966cdeffb0b2febae970903
danielfobooss <dani.fortuna97@gmail.com>
2021-06-15 04:14:45 +0100

    Update base.py

--- mutpy/operators/base.py
+++ mutpy/operators/base.py
@@ -54,8 +54,9 @@ class MutationOperator:
                     new_node = visitor(node)
                     self.visitor = visitor.__name__
                     self.current_node = node
-                    self.fix_node_internals(node, new_node)
-                    ast.fix_missing_locations(new_node)
+                    if new_node is not None:
+                        self.fix_node_internals(node, new_node)
+                        ast.fix_missing_locations(new_node)
                     yield new_node
                 except MutationResign:
                     pass
